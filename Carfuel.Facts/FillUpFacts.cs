70cf3dd272f18d225bf2a9b02e76f33d32276cef
atiwatc <aun_cpe_mut@hotmail.com>
2015-06-13 14:26:15 +0700

    add fact project

---
+++ Carfuel.Facts/FillUpFacts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carfuel.Models;
+using Xunit;
+
+
+namespace Carfuel.Facts
+{
+    public class FillUpFacts
+    {
+        public class GeneralUsage
+        {
+            [Fact]
+            public void Basic()
+            {
+                //Arrange
+                var f1 = new FillUp();
+                f1.Odometer = 1000;
+                f1.IsFull = true;
+                f1.Liters = 50.0;
+
+                //Act
+
+
+                //Assert
+                Assert.Equal(1000,f1.Odometer);
+                Assert.Equal(true,f1.IsFull);
+                Assert.Equal(50.0,f1.Liters);
+            }
+
+            [Fact]
+            public void DerfaultDateShouldBeNow()
+            {
+                //Arrange
+                var f = new FillUp();
+
+                //Act
+                DateTime dt = f.Date;
+
+                //Assert
+                Assert.Equal(expected: DateTime.Now, actual:dt);
+            }
+        }
+    }
+}
